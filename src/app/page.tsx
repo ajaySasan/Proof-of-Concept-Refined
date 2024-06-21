27338a2ce593e8093ed90ad8d356bee2ca7eb677
Ajay Sasan <ajay.sasan@blackdice.ai>
2024-06-21 09:27:49 +0100

    token seperate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,9 @@
 import axios from "axios";
 import Home from "./home";
+import Token from "./token";
 
-const Page = async () => {
-  try {
-    const operatorTokenResponse = await axios.post(
-      "https://api-pov.blackdice.ai/op/auth/login",
-      {
-        email: "operator@demo.com",
-        pass: "123456",
-      }
-    );
-
-    const uiToken = await axios.post("https://api-pov.blackdice.ai/pa/auth", {
-      email: "te@demo.com",
-      pass: "123456",
-    });
-
-    console.log("Operator Token:", operatorTokenResponse.data);
-    console.log("PA Token:", uiToken.data.token);
-
-    return (
-      <Home token={operatorTokenResponse.data} paToken={uiToken.data.token} />
-    );
-  } catch (err) {
-    console.log(err);
-    // Handle error or return fallback UI
-    return <div>Error loading data</div>;
-  }
+const Page = () => {
+  return <Token />;
 };
 
 export default Page;
---
+++ src/app/token.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import Home from "./home";
+
+const Token = async () => {
+  try {
+    const operatorTokenResponse = await axios.post(
+      "https://api-pov.blackdice.ai/op/auth/login",
+      {
+        email: "operator@demo.com",
+        pass: "123456",
+      }
+    );
+
+    const uiToken = await axios.post("https://api-pov.blackdice.ai/pa/auth", {
+      email: "te@demo.com",
+      pass: "123456",
+    });
+
+    console.log("Operator Token:", operatorTokenResponse.data);
+    console.log("PA Token:", uiToken.data.token);
+
+    return (
+      <Home token={operatorTokenResponse.data} paToken={uiToken.data.token} />
+    );
+  } catch (err) {
+    console.log(err);
+    // Handle error or return fallback UI
+    return <div>Error loading data</div>;
+  }
+};
+
+export default Token;
